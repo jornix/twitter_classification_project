556bd5d0d2000a59a25caeb6dc51db4fc7cc33e2
Jorn Pedersen <jornped@gmail.com>
2021-02-17 20:18:32 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
-
+from sklearn.naive_bayes import MultinomialNB
 
 
 # Import tweets from New York, London and Paris
